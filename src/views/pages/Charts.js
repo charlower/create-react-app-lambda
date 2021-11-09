ef79033cffc089d842782c65aca84a4e00042f9b
Charlie <charlows@gmail.com>
2021-11-09 14:48:38 +1100

    delete

--- src/views/pages/Charts.js
+++ src/views/pages/Charts.js
@@ -16,24 +16,7 @@
 */
 // javascipt plugin for creating charts
 import Chart from "chart.js";
-// react plugin used to create charts
-import { Line, Bar, Doughnut, Pie } from "react-chartjs-2";
-// reactstrap components
-import { Card, CardHeader, CardBody, Container, Row, Col } from "reactstrap";
-// core components
-import CardsHeader from "components/Headers/CardsHeader.js";
-
-import {
-  chartOptions,
-  parseOptions,
-  chartExample2,
-  chartExample3,
-  chartExample4,
-  chartExample5,
-  chartExample6,
-  chartExample7,
-} from "variables/charts.js";
-
+// react plugin used to create chart
 function Charts() {
   if (window.Chart) {
     parseOptions(Chart, chartOptions());
