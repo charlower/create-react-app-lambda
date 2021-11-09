ade90023017c12c1448ec915df9db51ab7528085
Charlie <charlows@gmail.com>
2021-11-09 13:20:36 +1100

    asdfsadf

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -19,8 +19,8 @@ import React from "react";
 import { useLocation, Route, Switch, Redirect } from "react-router-dom";
 // core components
 import AdminNavbar from "../components/Navbars/AdminNavbar.js";
-import AdminFooter from "./components/Footers/AdminFooter.js";
-import Sidebar from "./components/Sidebar/Sidebar.js";
+import AdminFooter from "../components/Footers/AdminFooter.js";
+import Sidebar from "../components/Sidebar/Sidebar.js";
 
 import routes from "../routes.js";
 
