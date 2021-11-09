ba2280cdd8cdd96e66a14ad6ac52dc3a1a83541e
Charlie <charlows@gmail.com>
2021-11-09 15:08:26 +1100

    delete

--- src/index.js
+++ src/index.js
@@ -29,9 +29,9 @@ import "select2/dist/css/select2.min.css";
 import "quill/dist/quill.core.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 // plugins styles downloaded
-// import "assets/vendor/nucleo/css/nucleo.css";
+import "./assets/vendor/nucleo/css/nucleo.css";
 // core styles
-// import "assets/scss/argon-dashboard-pro-react.scss?v1.2.0";
+import "./assets/scss/argon-dashboard-pro-react.scss?v1.2.0";
 
 import AdminLayout from "./layouts/Admin.js";
 import AuthLayout from "./layouts/Auth.js";
@@ -40,9 +40,9 @@ import IndexView from "./views/Index.js";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      {/* <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
       <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
-      <Route path="/" render={(props) => <IndexView {...props} />} /> */}
+      <Route path="/" render={(props) => <IndexView {...props} />} />
       <Redirect from="*" to="/" />
     </Switch>
   </BrowserRouter>,
