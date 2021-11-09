b67e91eb87a10f9607ef7ba186987fdda9514ee0
Charlie <charlows@gmail.com>
2021-11-09 12:40:14 +1100

    imports

--- src/index.js
+++ src/index.js
@@ -34,8 +34,8 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 // import "assets/scss/argon-dashboard-pro-react.scss?v1.2.0";
 
 import AdminLayout from "./layouts/Admin.js";
-// import AuthLayout from "layouts/Auth.js";
-// import IndexView from "views/Index.js";
+import AuthLayout from "./layouts/Auth.js";
+import IndexView from "./views/Index.js";
 
 ReactDOM.render(
   <BrowserRouter>
